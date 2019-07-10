b5e2e384b2f002680320655c9885b5f1551a3196
Benjamin Onwenu <bonwenu@emich.edu>
2019-07-09 22:17:23 -0500

    Updated backend

--- src/main/java/com/revature/util/LoginConrollerHelper.java
+++ src/main/java/com/revature/util/LoginConrollerHelper.java
@@ -1,5 +1,14 @@
 package com.revature.util;
 
-public class LoginConrollerHelper {
+import com.revature.model.User;
 
+public class LoginConrollerHelper {
+	public static boolean loginCheckIfUser(User u, User user) {
+		if(user == null)
+			return false;
+		if(u.getUsername().equals(user.getUsername()) && u.getPassword().equals(user.getPassword())) {
+			return true;
+		}
+		return false;
+	}
 }
