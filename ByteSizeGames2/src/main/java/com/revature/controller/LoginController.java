1f19bf5cb392d21dddf04239d12c32b5b9fe17fb
anthonyjlatorre <50970971+anthonyjlatorre@users.noreply.github.com>
2019-07-09 19:46:46 -0500

    Updated back end to send responses to front end

--- ByteSizeGames2/src/main/java/com/revature/controller/LoginController.java
+++ ByteSizeGames2/src/main/java/com/revature/controller/LoginController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.User;
 import com.revature.service.UserService;
+import com.revature.util.LoginConrollerHelper;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -38,11 +38,12 @@ public class LoginController {
 	
 	@PostMapping()
 	public User addUser(@RequestBody User u) {
+		System.out.println(u.toString());
 		User user = uService.findUserByUsername(u.getUsername());
-		if(u.getUsername().equals(user.getUsername()) && u.getPassword().equals(user.getPassword())) {
+		//System.out.println(user.toString());
+		if(LoginConrollerHelper.loginCheckIfUser(u, user)) {
 			return user;
 		}
-		System.out.println(user.toString()); 
 		return u;
 	}
 }
